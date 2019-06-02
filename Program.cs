64219381000254aac0d199bd5ffba9f4f0478431
Alex Pacheco <alex.pacheco.s@gmail.com>
2019-06-01 21:25:57 -0400

    Manejando arreglos

--- Program.cs
+++ Program.cs
@@ -7,29 +7,47 @@ namespace Etapa1
     {
         static void Main(string[] args)
         {
-            var escuela = new Escuela("Mi Escuela", 2010, ciudad:"Santiago");
+            var escuela = new Escuela("Mi Escuela", 2010, ciudad: "Santiago");
             escuela.Pais = "Chile";
             escuela.TipoEscuela = 0;
 
-            var curso1 = new Curso()
-            {
-                Nombre = "101"
-            };
+            var cursos = new Curso[3];
+
+            cursos[0] = new Curso() { Nombre = "101" };
+
             var curso2 = new Curso()
             {
                 Nombre = "201"
             };
-            var curso3 = new Curso()
-            {
-                Nombre = "301"
-            };
+
+            cursos[1] = curso2;
+
+            cursos[2] = new Curso { Nombre = "301" };
 
             Console.WriteLine(escuela);
             Console.WriteLine("===============");
-            Console.WriteLine($"Curso: {curso1.Nombre}, ID: {curso1.UniqueId}");
-            Console.WriteLine($"Curso: {curso2.Nombre}, ID: {curso2.UniqueId}");
-            Console.WriteLine($"Curso: {curso3.Nombre}, ID: {curso3.UniqueId}");
 
+            ImprimirCursos(cursos);
+            Console.WriteLine("===============");
+            ImprimirCursosForEach(cursos);
+        }
+
+        private static void ImprimirCursosForEach(Curso[] cursos)
+        {
+            foreach (var curso in cursos)
+            {
+                Console.WriteLine($"Curso: {curso.Nombre}, ID: {curso.UniqueId}");
+            }
+        }
+
+        private static void ImprimirCursos(Curso[] cursos)
+        {
+            int i = 0;
+            while (i < cursos.Length)
+            {
+                Console.WriteLine($"Curso: {cursos[i].Nombre}, ID: {cursos[i].UniqueId}");
+                i++;
+            }
         }
     }
 }
